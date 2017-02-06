00c74810fd9a941d52dd3d7295d03698731d2584
ChangJunPark <pcj006@gmail.colm>
2017-02-06 11:02:35 +0000

    cdplayer cd 유무 및 3가지 상태

--- ch04/state/cdplayer.c
+++ ch04/state/cdplayer.c
@@ -9,11 +9,24 @@ static const STATE *stopPlay();
 static const STATE *pausePlay();
 static const STATE *resumePlay();
 
+/* state    | state 1   | state 2   |
+ * ----------------------------------
+ * IDLE     | IDLE      | CD O      |
+ * IDLE     | IDEL      | CD    X   |
+ * PLAY     | PLAY      | CD O      |
+ * N/A      | PLAYE     | CD    X   |
+ * PAUSE    | PAUSE     | CD O      |
+ * N/A      | PAUSE     | CD    X   |
+ */
+
+
+// 각 STATE 별 event 함수들 할당
 // STATE 구조체 바로 생성
 // STATE 변수명 = {*함수, *함수};
 const STATE IDLE = {ignore, startPlay};
 const STATE PLAY = {stopPlay, pausePlay};
 const STATE PAUSE = {stopPlay, resumePlay};
+const STATE N_A = {ignore, ignore};
 
 void initialize(){
     pCurrentState = &IDLE;
--- ch04/state/cdplayer.h
+++ ch04/state/cdplayer.h
@@ -4,7 +4,12 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+/*
+ * State Pattern -> state가 2개 이상일 때
+ * STATE 구조체를 만들고
+ * 구조체 내에는 STATE 구조체를 반환하는 가상함수를 정의함.
+ * 정의하는 가상함수는 각 상태에서의 이벤트를 뜻함.
+ */
 typedef struct State {
     const struct State *(* const stop)();
     const struct State *(* const playOrPause)();
@@ -17,5 +22,4 @@ void onPlayOrPause();
 #ifdef __cplusplus
 }
 #endif
-
 #endif
